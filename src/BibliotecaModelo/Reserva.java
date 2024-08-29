da7ed55286c66ad987c9f6a1e52f352c8084c954
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-29 16:00:55 -0300

    Update Reserva.java

--- src/BibliotecaModelo/Reserva.java
+++ src/BibliotecaModelo/Reserva.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package BibliotecaModelo;
 
 import UsuarioModelo.Usuario;
@@ -10,8 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- *
- * @author theou
+ Classe de definição de reserva
  */
 public class Reserva {
    private Usuario usuario;
