5df3a64c6ee6c7160bed010859ae0127d20b483c
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-30 11:22:04 -0300

    Update Repositorio.java

--- src/Sistema/Repositorio.java
+++ src/Sistema/Repositorio.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Definição da classe Repositório.
+ * Padrão Singleton.
  */
 package Sistema;
 
@@ -15,8 +14,7 @@ import UsuarioModelo.Usuario;
 import java.util.ArrayList;
 
 /**
- *
- * @author theou
+ * contém as listas de usuarios, livros, e observadores
  */
 public class Repositorio {
     
