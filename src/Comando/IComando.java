a02a5e0bd15ee4fd31aad5908090012e86f5ee3f
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-30 10:51:24 -0300

    Update IComando.java

--- src/Comando/IComando.java
+++ src/Comando/IComando.java
@@ -1,14 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Definição da interface IComando
+ * Padrão Command.
  */
 package Comando;
 
-/**
- *
- * @author theou
- */
 public interface IComando {
     public void executar(Parametro p);
 }
