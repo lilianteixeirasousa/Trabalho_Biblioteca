33450e1a46dca265d3330fa94da48739f8fdea79
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-30 11:12:39 -0300

    Update Main.java

--- src/Sistema/Main.java
+++ src/Sistema/Main.java
@@ -1,17 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Definição da classe main.
  */
 package Sistema;
 
 import java.util.Scanner;
 
 
-/**
- *
- * @author theou
- */
 public class Main {
     public static void main(String[] args) {
         Repositorio repositorio = Repositorio.pegarInstancia();
