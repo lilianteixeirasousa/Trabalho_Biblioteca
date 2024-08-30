c4a04179406742a317d061e6a96a7927f283781d
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-30 10:53:05 -0300

    Update Parametro.java

--- src/Comando/Parametro.java
+++ src/Comando/Parametro.java
@@ -1,13 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Definição da classe parâmetro.
  */
 package Comando;
 
 /**
- *
- * @author theou
+ * instancia 
  */
 public class Parametro {
     private String primeiroParametro;
