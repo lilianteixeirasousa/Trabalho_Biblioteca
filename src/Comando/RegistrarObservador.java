9d6e01b335eef4cb1cec3dae975b68089a0c784d
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-30 10:58:55 -0300

    Update RegistrarObservador.java

--- src/Comando/RegistrarObservador.java
+++ src/Comando/RegistrarObservador.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Definição da Classe RegistrarObservador
  */
 package Comando;
 
@@ -11,8 +9,7 @@ import Sistema.Trabalhador;
 import UsuarioModelo.IObservador;
 
 /**
- *
- * @author theou
+ * implementa a interface IComando
  */
 public class RegistrarObservador implements IComando{
 
