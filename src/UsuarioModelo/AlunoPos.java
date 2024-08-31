4002b4beb574d0e1a821907c678b69288a8f3e5e
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-31 15:04:20 -0300

    Update AlunoPos.java

--- src/UsuarioModelo/AlunoPos.java
+++ src/UsuarioModelo/AlunoPos.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Define classe Aluno Pos(graduação).
  */
 package UsuarioModelo;
 
-/**
- *
- * @author theou
- */
 public class AlunoPos extends Usuario {
     
     public AlunoPos (String codigo, String nome) {
