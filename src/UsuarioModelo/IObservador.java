a7fa42e7bc014d1cbc06303b5cea800975968f18
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-31 15:05:02 -0300

    Update IObservador.java

--- src/UsuarioModelo/IObservador.java
+++ src/UsuarioModelo/IObservador.java
@@ -1,14 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Define interface de observador.
+ * Padrão Observer.
  */
 package UsuarioModelo;
 
-/**
- *
- * @author theou
- */
 public interface IObservador {
     public void atualizar();
     public String consultarObservador();
