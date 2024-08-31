03dbeea1c4497add8bd0c1f1507540079c6b0cd7
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-31 15:05:35 -0300

    Update IRegra.java

--- src/UsuarioModelo/IRegra.java
+++ src/UsuarioModelo/IRegra.java
@@ -1,16 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Define interface IRegra.
  */
 package UsuarioModelo;
 
 import BibliotecaModelo.Livro;
 
-/**
- *
- * @author theou
- */
 public interface IRegra {
     public boolean condicoesEmprestimo(Usuario usuario, Livro livro);
 }
