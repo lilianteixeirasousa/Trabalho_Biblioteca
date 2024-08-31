a80e46a8241e2fdfa3b8f021ff4818c8f5e2dfc1
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-31 15:03:42 -0300

    Update AlunoGrad.java

--- src/UsuarioModelo/AlunoGrad.java
+++ src/UsuarioModelo/AlunoGrad.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Define classe AlunoGrad(uação).
  */
 package UsuarioModelo;
 
-/**
- *
- * @author theou
- */
 public class AlunoGrad extends Usuario {
     
     public AlunoGrad(String codigo, String nome) {
