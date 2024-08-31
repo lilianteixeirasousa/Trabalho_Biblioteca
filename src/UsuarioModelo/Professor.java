9d92170aec6c20ccfb4398d8557c886d27d3ec2a
lilianteixeirasousa <43791589+lilianteixeirasousa@users.noreply.github.com>
2024-08-31 15:06:05 -0300

    Update Professor.java

--- src/UsuarioModelo/Professor.java
+++ src/UsuarioModelo/Professor.java
@@ -1,16 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Define classe Professor.
  */
 package UsuarioModelo;
 
 import Sistema.Repositorio;
 
-/**
- *
- * @author theou
- */
 public class Professor extends Usuario implements IObservador{
     
     private int vezesNotificado;
